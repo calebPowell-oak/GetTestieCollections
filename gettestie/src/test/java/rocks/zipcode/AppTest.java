24df1de05447643ebd6afcb0b2c04ed4cfb2d6ab
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-06 19:40:09 -0400

    Completed tests for priorityQueue

--- gettestie/src/test/java/rocks/zipcode/AppTest.java
+++ gettestie/src/test/java/rocks/zipcode/AppTest.java
@@ -1,6 +1,7 @@
 package rocks.zipcode;
 
 import com.sun.tools.hat.internal.model.HackJavaValue;
+import jdk.internal.org.objectweb.asm.tree.InnerClassNode;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -117,11 +118,80 @@ public class AppTest {
     @Test
     public void vectorTest() {
         // Given
+        Vector<Integer> myVec = new Vector<>();
+        Integer[] arr = {1,2,3,4,5,6,7,8,9};
+        myVec.addAll(Arrays.asList(arr));
+        myVec.add(4, 11);
 
         // When
-        Integer actual = myDQ.removeLast();
+        Integer actual = myVec.size();
 
         // Assert
-        Assert.assertTrue(actual.equals(9));
+        Assert.assertTrue(actual.equals(10));
     }
+
+    @Test
+    public void treeMapTest() {
+        // Given
+        Map<Integer, String> myTM = new TreeMap<>();
+        Integer[] ints = {1,3,2,4,5,5};
+        String[] strs = {"one", "two", "three", "four", "five","yolo"};
+        for(int i = 0; i < ints.length; i++){
+            myTM.put(ints[i], strs[i]);
+        }
+
+        // When
+        Integer actual = myTM.size();
+        Integer expected = 5;
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+        System.out.println(myTM.keySet().toString()); //ordered in ascending order.
+    }
+
+    @Test
+    public void stackTest() {
+        // Given
+        Collection<Integer> stack = new Stack<>();
+        Integer[] nums = {1,6,4,7,5,6,9,65,3};
+        stack.addAll(Arrays.asList(nums));
+
+        // When
+        Integer expected = 99930;
+        ((Stack<Integer>) stack).push(expected);
+
+        // Assert
+        Assert.assertEquals(expected, ((Stack<Integer>) stack).pop());
+    }
+
+    @Test
+    public void treeSetTest() {
+        // Given
+        Collection<Integer> treeSet = new TreeSet<>();
+        Integer[] nums = {1,6,4,7,5,6,9,65,3};
+        treeSet.addAll(Arrays.asList(nums));
+        Integer expected = 65;
+
+        // When
+        Integer actual = ((TreeSet<Integer>) treeSet).last();
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priorityQueueTest() {
+        // Given
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Integer[] num = {18646,346,7,4,3,5,6,6,45,9};
+        pq.addAll(Arrays.asList(num));
+
+        // When
+        Integer expected = pq.peek();
+
+        // Assert
+        Assert.assertTrue(expected.equals(3));
+    }
+
+
 }
